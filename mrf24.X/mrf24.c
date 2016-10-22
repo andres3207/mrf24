157bab3f8c2266fd35c794df2fcb5a8ea1b9fe0c
Andres Gomez <andres3207@gmail.com>
2016-10-22 15:19:13 -0300

    Spi funcionando

---
+++ mrf24.X/mrf24.c
@@ -0,0 +1,146 @@
+/* 
+ * File:   mrf24.c
+ * Author: andres
+ *
+ * Created on 22 de octubre de 2016, 14:30
+ */
+
+// PIC18F45K20 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1H
+#pragma config FOSC = INTIO67   // Oscillator Selection bits (Internal oscillator block, port function on RA6 and RA7)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = ON        // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 18        // Brown Out Reset Voltage bits (VBOR set to 1.8 V nominal)
+
+// CONFIG2H
+#pragma config WDTEN = OFF      // Watchdog Timer Enable bit (WDT is controlled by SWDTEN bit of the WDTCON register)
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config HFOFST = ON      // HFINTOSC Fast Start-up (HFINTOSC starts clocking the CPU without waiting for the oscillator to stablize.)
+#pragma config MCLRE = OFF      // MCLR Pin Enable bit (RE3 input pin enabled; MCLR disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = ON         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection Block 0 (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection Block 1 (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection Block 2 (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection Block 3 (Block 3 (006000-007FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection Block 0 (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection Block 1 (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection Block 2 (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection Block 3 (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection Block 0 (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection Block 1 (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection Block 2 (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection Block 3 (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0007FFh) not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+void Port_Config(void);
+void SPI_Config(void);
+
+unsigned i=0;
+unsigned dato=0;
+
+int main(int argc, char** argv) {
+
+    Port_Config();
+    SPI_Config();
+    
+    while(1){
+        _delay3(255);
+        _delay3(255);
+        _delay3(255);
+        LATD++;
+        i++;
+        SSPBUF=i;
+    }
+
+    return (EXIT_SUCCESS);
+}
+
+/************ CONFIGURACIONES ****************/
+
+void Port_Config(void){
+    TRISD=0;
+    LATD=0;
+}
+
+void SPI_Config(void){
+    SSPSTAT=0x00;
+    SSPCON1=0;
+    SSPCON1bits.SSPEN=1;
+    TRISCbits.RC3=0;
+    TRISCbits.RC4=1;
+    TRISCbits.RC5=0;
+    
+    INTCON=0xc0;
+    PIE1bits.SSPIE=1;
+}
+
+/*********************************************/
+
+
+
+
+/************ INTERRUPCIONES ****************/
+void interrupt interrupciones(void)
+{
+ if (TMR0IE && TMR0IF) {
+ TMR0IF=0;
+ return;
+ }
+ 
+ 
+ if (SSPIE && SSPIF) {
+ SSPIF=0;
+ dato=SSPBUF;
+ return;
+ }
+ 
+  
+ // process other interrupt sources here, if required
+}
+/*******************************************/
---
+++ mrf24.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=mrf24.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/mrf24.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mrf24.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mrf24.x.tar
